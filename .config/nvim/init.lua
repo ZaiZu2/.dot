7f272f504f715e611047ae4420bf46a0065c2636
Jakub Kawecki <jakub@forloop.ai>
2024-10-15 22:05:08 +0200

    Add nvim.lua support - project specific script allowing to inject variables into nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,19 @@
+-- Read `init.lua` from cwd, and save the result to `vim.g.custom`
+-- `vim.g.custom` can be used to modify lua initialization
+local init_file = vim.fn.getcwd() .. '/nvim.lua'
+if vim.fn.filereadable(init_file) == 1 then
+  local ok, custom = pcall(dofile, init_file)
+  if ok then
+    if type(custom) == 'table' then
+      vim.g.custom = custom
+    else
+      vim.notify('vim.init must return a table' .. custom, vim.log.levels.ERROR)
+    end
+  else
+    vim.notify('Error loading `nvim.lua`: ' .. custom, vim.log.levels.ERROR)
+  end
+end
+
 -- :help lua-guide
 -- https://neovim.io/doc/user/lua-guide.html
 -- :help mapleader
