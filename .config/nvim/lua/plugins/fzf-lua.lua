b65d477ab1b1fc88fc4e7f23fa08157abab2f196
Jakub Kawecki <jakkawecki@gmail.com>
2025-02-23 18:38:52 +0100

    feat: add keymaps to fzflua

--- .config/nvim/lua/plugins/fzf-lua.lua
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -8,23 +8,30 @@ return {
             fzflua.setup {
                 keymap = {
                     builtin = {
-                        true,
-                        ['ctrl-d'] = 'preview-page-down',
-                        ['ctrl-u'] = 'preview-page-up',
+                        ['<F1>'] = 'toggle-help',
+                        ['<F2>'] = 'toggle-fullscreen',
+                        -- Only valid with the 'builtin' previewer
+                        ['<F3>'] = 'toggle-preview-wrap',
+                        ['<F4>'] = 'toggle-preview',
+                        ['<F5>'] = 'toggle-preview-ccw',
+                        ['<F6>'] = 'toggle-preview-cw',
+                        ['<C-d>'] = 'preview-page-down',
+                        ['<C-u>'] = 'preview-page-up',
+                        ['<S-left>'] = 'preview-page-reset',
                     },
                     fzf = {
-                        true,
-                        ['ctrl-d'] = 'half-page-down',
-                        ['ctrl-u'] = 'half-page-up',
+                        ['ctrl-f'] = 'half-page-down',
+                        ['ctrl-b'] = 'half-page-up',
+                        ['ctrl-a'] = 'beginning-of-line',
+                        ['ctrl-e'] = 'end-of-line',
+                        ['alt-a'] = 'toggle-all',
+                        -- Only valid with fzf previewers (bat/cat/git/etc)
+                        ['f3'] = 'toggle-preview-wrap',
+                        ['f4'] = 'toggle-preview',
+                        ['ctrl-d'] = 'preview-page-down',
+                        ['ctrl-u'] = 'preview-page-up',
+                        ['ctrl-q'] = 'select-all+accept',
                     },
-                },
-                actions = {},
-                files = {
-                    -- winopts = fullscreen_opts,
-                    formatter = 'path.filename_first',
-                },
-                grep = {
-                    -- winopts = fullscreen_opts
                 },
             }
 
