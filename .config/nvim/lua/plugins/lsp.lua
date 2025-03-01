8f09f17e5080d9e7c965a0740c5f10bc1105dc44
Jakub Kawecki <jakkawecki@gmail.com>
2025-03-01 19:26:38 +0100

    feat: add mdformat to automatic formatters in nvim

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -22,10 +22,10 @@ return {
         config = function()
             -- Specify all language tools to be installed automatically
             local linters = { 'shellcheck', 'hadolint' }
-            local formatters = { 'stylua', 'shfmt' }
+            local formatters = { 'stylua', 'shfmt', 'mdformat' }
             local daps = { 'debugpy' }
             local lsp_servers = { -- :help lspconfig-all
-                ts_ls = {},
+                -- ts_ls = {},
                 basedpyright = {
                     settings = {
                         basedpyright = {
