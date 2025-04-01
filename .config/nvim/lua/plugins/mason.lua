c107b20d4c15f13dbff816f9bcba3d5b3ed3b64c
Jakub Kawecki <jakub.kawecki@absa.africa>
2025-04-01 17:17:52 +0200

    feat: Ignore commented-out LSP files

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -22,9 +22,15 @@ return {
             -- Recognize LSP config files and enable corresponding LSPs
             local lsp_path = vim.fn.stdpath 'config' .. '/lsp/'
             local lsps = {} ---@type string[]
-            for file_path in vim.fs.dir(lsp_path, {}) do
-                table.insert(lsps, vim.fs.basename(file_path):match '^[^%.]+')
+            for file_name in vim.fs.dir(lsp_path, {}) do
+                local lsp_name = vim.fs.basename(file_name):match '^[^%.]+'
+                local lsp_conf = require('lsp.' .. lsp_name)
+                -- Ignore commented-out LSP files (empty modules)
+                if lsp_conf ~= true then
+                    table.insert(lsps, lsp_name)
+                end
             end
+            vim.print(lsps)
             vim.lsp.enable(lsps)
 
             local config = require 'config'
