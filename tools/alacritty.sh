2465a638f26c2cb156e32fd8a837afb56dd208e6
Jakub Kawecki <jakkawecki@gmail.com>
2025-05-27 22:55:28 +0200

    feat: add alacritty, remove ansible files

---
+++ tools/alacritty.sh
@@ -0,0 +1,36 @@
+is_installed_alacritty() {
+  command -v alacritty >/dev/null 2>&1
+}
+
+install_linux() {
+  local alacritty_url="https://github.com/alacritty/alacritty.git"
+  local alacritty_repo="$XDG_DATA_HOME/alacritty"
+  blue "Cloning repo $alacritty_url to $alacritty_repo"
+  git clone --depth 1 "$alacritty_url" "$alacritty_repo" || {
+    fail "Failed to clone $alacritty_url"
+    return 1
+  }
+  pushd "$alacritty_url"
+  cargo build --release
+  cp 'extra/logo/alacritty-term.svg' '/usr/share/pixmaps/Alacritty.svg'
+  desktop-file-install 'extra/linux/Alacritty.desktop'
+  update-desktop-database
+  popd
+  blue "Symlinking ALACRITTY binary"
+  ln -sf "$alacritty_url/target/release/alacritty" "$XDG_BIN_HOME/alacritty"
+}
+
+install_darwin() {
+  brew install alacritty || {
+    fail "Failed to install alacritty"
+    return 1
+  }
+}
+
+install_alacritty() {
+  if [ "$OS" = 'darwin' ]; then
+    install_darwin || return 1
+  elif [ "$OS" = 'linux' ]; then
+    install_linux || return 1
+  fi
+}
