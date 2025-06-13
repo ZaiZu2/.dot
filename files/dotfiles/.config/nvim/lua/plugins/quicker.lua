6874ec1c9a416170816446fc02a037b662d00fc8
Jakub Kawecki <jakkawecki@gmail.com>
2025-06-13 19:50:09 +0200

    feat: add quicker.nvim

---
+++ files/dotfiles/.config/nvim/lua/plugins/quicker.lua
@@ -0,0 +1,40 @@
+return {
+    {
+        'stevearc/quicker.nvim',
+        event = 'FileType qf',
+        config = function()
+            require('quicker').setup()
+
+            vim.keymap.set('n', '<leader>q', function()
+                require('quicker').toggle()
+            end, {
+                desc = 'Toggle quickfix',
+            })
+            vim.keymap.set('n', '<leader>l', function()
+                require('quicker').toggle { loclist = true }
+            end, {
+                desc = 'Toggle loclist',
+            })
+            ---@module "quicker"
+            ---@type quicker.SetupOptions
+            require('quicker').setup {
+                keys = {
+                    {
+                        '>',
+                        function()
+                            require('quicker').expand { before = 2, after = 2, add_to_existing = true }
+                        end,
+                        desc = 'Expand quickfix context',
+                    },
+                    {
+                        '<',
+                        function()
+                            require('quicker').collapse()
+                        end,
+                        desc = 'Collapse quickfix context',
+                    },
+                },
+            }
+        end,
+    },
+}
