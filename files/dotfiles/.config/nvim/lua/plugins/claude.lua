a8704f66c6fd85419b7d3b1c1d1014163a6420ef
Jakub Kawecki <jakkawecki@gmail.com>
2026-07-01 13:36:18 +0200

    ding dong

--- files/dotfiles/.config/nvim/lua/plugins/claude.lua
+++ files/dotfiles/.config/nvim/lua/plugins/claude.lua
@@ -1,4 +1,64 @@
 return {
+    -- {
+    --     'nickjvandyke/opencode.nvim',
+    --     dependencies = {
+    --         {
+    --             -- `snacks.nvim` integration is recommended, but optional
+    --             ---@module "snacks" <- Loads `snacks.nvim` types for configuration intellisense
+    --             'folke/snacks.nvim',
+    --             optional = true,
+    --             opts = {
+    --                 input = {}, -- Enhances `ask()`
+    --                 picker = { -- Enhances `select()`
+    --                     actions = {
+    --                         opencode_send = function(...) return require('opencode').snacks_picker_send(...) end,
+    --                     },
+    --                     win = {
+    --                         input = {
+    --                             keys = {
+    --                                 ['<a-a>'] = { 'opencode_send', mode = { 'n', 'i' } },
+    --                             },
+    --                         },
+    --                     },
+    --                 },
+    --             },
+    --         },
+    --     },
+    --     config = function()
+    --         ---@type opencode.Opts
+    --         vim.g.opencode_opts = {
+    --             -- Your configuration, if any; goto definition on the type or field for details
+    --         }
+    --
+    --         vim.o.autoread = true -- Required for `opts.events.reload`
+    --
+    --         -- Recommended/example keymaps
+    --         vim.keymap.set(
+    --             { 'n', 'x' },
+    --             '<leader>cA',
+    --             function() require('opencode').ask('@this: ', { submit = true }) end,
+    --             { desc = 'Ask opencode…' }
+    --         )
+    --         vim.keymap.set(
+    --             { 'n', 'x' },
+    --             '<leader>cb',
+    --             function() return require('opencode').operator '@buffer ' end,
+    --             { desc = '[a]dd current buffer' }
+    --         )
+    --         vim.keymap.set(
+    --             { 'n', 'x' },
+    --             '<leader>ca',
+    --             function() return require('opencode').operator '@this ' end,
+    --             { desc = '[a]dd range to opencode', expr = true }
+    --         )
+    --         vim.keymap.set(
+    --             'n',
+    --             '<leader>ca',
+    --             function() return require('opencode').operator '@this ' .. '_' end,
+    --             { desc = '[a]dd line to opencode', expr = true }
+    --         )
+    --     end,
+    -- },
     {
         'coder/claudecode.nvim',
         dependencies = { 'folke/snacks.nvim' },
